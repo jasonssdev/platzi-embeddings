f5be6dc075e5ef2efb1a360ceaeb2da36506eeec
Jason Dev <jasonssdev@gmail.com>
2025-05-02 21:25:26 -0500

    project initial setup

---
+++ paths.py
@@ -0,0 +1,15 @@
+from pathlib import Path
+
+# Project Root
+PROJECT_ROOT = Path(__file__).resolve().parent
+
+# Directories root
+DATA_DIR = PROJECT_ROOT / "data"
+RAW_DIR = DATA_DIR / "raw"
+DB_DIR = DATA_DIR / "db"
+DOCS_DIR = PROJECT_ROOT / "docs"
+IMAGES_DIR = DOCS_DIR / "images"
+HTML_DIR = RAW_DIR / "html"
+CSV_DIR = RAW_DIR / "csv"
+PDF_DIR = RAW_DIR / "pdf"
+TXT_DIR = RAW_DIR / "txt"
